fd87d3187ed7147127935a5e1e641e638c961414
alfeushersandy <alfeusputra@gmail.com>
2023-09-10 20:42:25 +0700

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary p-3 shadow-sm">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bolder" href="#">SandiNgulik</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex align-items-center justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item text-uppercase fw-bolder">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+          <li class="nav-item text-uppercase fw-bolder">
+            <a class="nav-link" aria-current="page" href="#">Artikel</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-uppercase fw-bolder" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Kategori
+            </a>
+            <ul class="dropdown-menu text-uppercase fw-bolder">
+              <li><a class="dropdown-item" href="#">Jaringan</a></li>
+              <li><a class="dropdown-item" href="#">Server</a></li>
+              <li><a class="dropdown-item" href="#">Pemrogaman</a></li>
+            </ul>
+          </li>
+      </div>
+    </div>
+  </nav>
